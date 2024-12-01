09ad98076f99a5447f25c19b50842bfa31d00b75
Ferosorio15 <mf.osorio15@gmail.com>
2024-12-01 22:33:44 +0000

    Excuse WIP

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,50 @@
 
 // This line of code will make sure the page is fully loaded
 window.onload = function () {
-    // Write the logic in this function
+  // Write the logic in this function
 
-    // 1. Get the excuse element
-    const excuse = document.getElementById('excuse');
+  // 1. Get the excuse element
+  const excuse = document.getElementById('excuse');
+  console.log(excuseText);
 
-    // 2. Generate a random excuse
+  // 2. Generate a random excuse
 
-    const excuseText = `The dog ate my homework for breakfast`;
+  const whoToBlame = [
+      "My pet Rooney",
+      "The new intern",
+      "The copy machine",
+      "My car was",
+      "I can not leave,",       
+    ];
+    const whatTheyDid = [
+      "chewed",
+      "deleted ",
+      "abducted",
+      "I been trapped",
+      "malfunctioned",        
+    ];
+    const whatToAvoid = [
+      "my laptop charger",
+      "my entire project",
+      "by aliens",
+      "in an endless loop of Zoom meetings",
+      "trapped my hand inside",        
+    ];
+    const WhenDidItHappen = [
+      "this morning.",
+      "yesterday.",
+      "a week ago",
+      "this Afternoon",
+      "2 hours ago",        
+    ];     
 
-    // 3. Inseart the excuse into the excuse element
-    excuse.innerHTML = excuseText;
+    const indexPart1 = Math.floor(Math.random() * whoToBlame.length); 
+    const indexPart2 = Math.floor(Math.random() * whatTheyDid.length);
+    const indexPart3 = Math.floor(Math.random() * whatToAvoid.length);
+    const indexPart4 = Math.floor(Math.random() * WhenDidItHappen.length);
+  
+  const excuseText = `${part1[indexPart1]} ${part2[indexPart2]} ${part2[indexPart3]} ${part2[indexPart4]}`;
+    
+  // 3. Inseart the excuse into the excuse element
+  excuse.innerHTML = excuseText;
 }
